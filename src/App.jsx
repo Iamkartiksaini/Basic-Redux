ec5a278eeae1bc817d9ab32520e77a3849f3cb35
Mr. Kartik Saini <104669343+Iamkartiksaini@users.noreply.github.com>
2023-01-04 21:55:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Component/Header";
+import First_vh from "./Pages/First_vh";
+import Second_vh from "./Pages/Second_vh";
+function App() {
+  return (
+    <div className="MainContainer">
+      <Header />
+      <First_vh />
+      <Second_vh />
+    </div>
+  );
+}
+
+export default App;
